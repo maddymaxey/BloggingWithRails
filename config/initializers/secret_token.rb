c2ad6179c6c0219d25052a637ff25ae6b4c2164b
MaddyMaxey <mmaxey0@gmail.com>
2013-06-24 22:44:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crated::Application.config.secret_token = 'f46081f4539731607567668d4aaa29ade8d2b88d5718bff29c6fbbcf8453e0c09cd67a5faffb4e4daa810f4e16860392675b6d5dd854e2eed3b8444d11dedbd3'
